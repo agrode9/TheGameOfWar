32ce406e491cb53896aead0a66dc3dd1cfb9ccf2
Grode <agrode@LAMU02T76BPGTFM.uhc.com>
2017-06-12 16:04:29 -0500

    initial

--- War.java
+++ War.java
@@ -58,15 +58,6 @@ public class War {
 		System.out.println("Player Two's deck size : " + playerTwo.size());
 	}
 
-//test
-
-
-
-
-
-
-
-//test2
 	/**
 	 *   This method initiates the game and makes the player decks
 	 */
