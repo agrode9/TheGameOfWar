d35e927cc7d544e7de15ba5e5f12ac1388faa96b
Grode <agrode@LAMU02T76BPGTFM.uhc.com>
2017-06-13 11:50:54 -0500

    add title and card #

--- WarGUI.java
+++ WarGUI.java
@@ -6,7 +6,7 @@ public class WarGUI extends Application {
   public void start(Stage primaryStage)
   {
     Button btn = new Button();
-    btn.setText("Welcome to the game of war");
+    btn.setText("Start new game");
     btn.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event){
@@ -14,8 +14,18 @@ public class WarGUI extends Application {
       }
     });
 
+    //will need to merge this and War.java
+    Label p1 = new Label("Player one has " + playerOne.size() + " cards.");
+    Label p2 = new Label("Player two has " + playerTwo.size() + " cards.");
+
+    //here we need to display the next card. Will need to be integratred with
+    //input
+    
+
     StackPane root = new StackPane();
     root.getChildren().add(btn);
+    root.getChildren().add(p1);
+    root.getChildren().add(p2);
 
     Scene scene = new Scene(root, 300, 250);
     primaryStage.setTitle("The Game Of War");
