1b159b875fa19994b5036986ad3081bcf55d1843
Bergan <nbergan@LAMU02T624AGTFM.uhc.com>
2017-06-15 16:25:35 -0500

    Player Card Reveal Logic

--- War.java
+++ War.java
@@ -1,16 +1,33 @@
 import java.util.*;
 
 public class War {
-	static List<Card> playerOneHand = null;
-	static List<Card> playerTwoHand = null;
+	public static List<Card> playerOneHand = null;
+	public static List<Card> playerTwoHand = null;
 	static ArrayList<Card> playerOneFacedownCards  = new ArrayList<Card>();
 	static ArrayList<Card> playerTwoFacedownCards  = new ArrayList<Card>();
 
 	public static void main( String[ ] args){
 		startGame();
 
-		printStart();
+//		printStart();
+		
+		
 
+		System.out.println("Player one hand size is "+ playerOneHand.size());
+		for(int j = 0; j < 26; j++)
+		{
+			System.out.print(playerOneHand.get(j).getName() + " ");
+		}
+		
+		System.out.println(" ");
+		System.out.println(" ");
+		
+		System.out.println("Player two hand size is "+ playerTwoHand.size());
+		for(int j = 0; j < 26; j++)
+		{
+			System.out.print(playerTwoHand.get(j).getName() + ",");
+		}
+		
 		Scanner input = new Scanner(System.in);
 		while(playerOneHand.size() != 0 && playerTwoHand.size() != 0)
 		{
@@ -25,35 +42,75 @@ public class War {
 			 * 	3) quit
 			 * Though this would work, definitely not as interactive or fun...
 			 */
-			getInput("Player One's card: ");
-			getInput("Player Two's card: ");
+			getInput();
 
-		Card playerOneCardLaid = playerOneHand.remove(0);
-		Card playerTwoCardLaid = playerTwoHand.remove(0);
+			
+			// THIS LINE THREW {java.util.ConcurrentModificationException}
+			
+			
+			Card playerOneCardLaid = playerOneHand.remove(0);
+			Card playerTwoCardLaid = playerTwoHand.remove(0);
 
-		if(playerOneCardLaid.getValue() > playerTwoCardLaid.getValue())
-		{
-			playerOneHand.add(playerOneCardLaid);
-			playerOneHand.add(playerTwoCardLaid);
-		}
-		else if(playerOneCardLaid.getValue() < playerTwoCardLaid.getValue())
-		{
-			playerTwoHand.add(playerOneCardLaid);
-			playerTwoHand.add(playerTwoCardLaid);
-		}
-		else
-		{
-			warHandler(playerOneCardLaid,playerTwoCardLaid);
-		}
+			if(playerOneCardLaid.getValue() > playerTwoCardLaid.getValue())
+			{
+				playerOneHand.add(playerOneCardLaid);
+				playerOneHand.add(playerTwoCardLaid);
+			}
+			else if(playerOneCardLaid.getValue() < playerTwoCardLaid.getValue())
+			{
+				playerTwoHand.add(playerOneCardLaid);
+				playerTwoHand.add(playerTwoCardLaid);
+			}
+			else
+			{
+				warHandler(playerOneCardLaid,playerTwoCardLaid);
+			}
 
-
+			
+			
 		}
 	}
 	/**
 	 * This method pulls in user input.
 	 */
-	public static void getInput(String message)
+	public static void getInput()
 	{
+		boolean p = false;
+		boolean q = false;
+		Scanner input = new Scanner(System.in);
+		
+		while(!p||!q)
+		{
+			String userInput = input.nextLine();
+			
+			if(userInput.equals("p"))
+			{
+				p = true;
+			}
+			
+			if(userInput.equals("q"))
+			{
+				q = true;
+			}
+			
+			if(p)
+			{
+				System.out.println("P1: " + playerOneHand.get(0).getName());
+			}
+			else
+			{
+				System.out.println("P1: ?");
+			}
+			
+			if(q)
+			{
+				System.out.println("P2: " + playerTwoHand.get(0).getName());
+			}
+			else
+			{
+				System.out.println("P2: ?");
+			}
+		}
 
 	}
 
@@ -77,9 +134,9 @@ public class War {
 		System.out.println("Player Two's deck size : " + playerTwoHand.size());
 	}
 
-/*
+	/*
 	This ends the game
-*/
+	 */
 	public static void gameOver()
 	{
 		System.out.println("GAME OVER");
@@ -113,6 +170,8 @@ public class War {
 		//       Collections.shuffle(shuffledDeck);
 		//now we need to split the shuffled deck and assign it t0 p1 and p2
 
+		// THIS ALSO SET OFF {java.util.ConcurrentModificationException}
+		
 		playerOneHand = shuffledDeck.subList(0,26); // First 26 cards assigned to Player 1 (Needs to be tested)
 		playerTwoHand = shuffledDeck.subList(26,52); // Last 26 cards assigned to Player 2 (Needs to be tested)
 
@@ -169,81 +228,81 @@ public class War {
 
 
 
-		/*
-		*	This recursive method calculates all war possibilities.
-		*	If player doesn't have 4 cartds for war, then they play their last card as the face up card.
-		*	If a player has war on their last card in their had, that player loses.
-		*
-		*/
-		public static void warHandler(Card playerOneCardLaid, Card playerTwoCardLaid)
+	/*
+	 *	This recursive method calculates all war possibilities.
+	 *	If player doesn't have 4 cartds for war, then they play their last card as the face up card.
+	 *	If a player has war on their last card in their had, that player loses.
+	 *
+	 */
+	public static void warHandler(Card playerOneCardLaid, Card playerTwoCardLaid)
+	{
+		Card playerOneWarCard = null;
+		Card playerTwoWarCard = null;
+
+		playerOneFacedownCards.add(playerOneCardLaid);
+		playerTwoFacedownCards.add(playerTwoCardLaid);
+
+		//Check to see if any player has no cards remaining
+		if(playerOneHand.isEmpty() || playerTwoHand.isEmpty())
 		{
-			Card playerOneWarCard = null;
-			Card playerTwoWarCard = null;
-
-			playerOneFacedownCards.add(playerOneCardLaid);
-			playerTwoFacedownCards.add(playerTwoCardLaid);
-
-			//Check to see if any player has no cards remaining
-			if(playerOneHand.isEmpty() || playerTwoHand.isEmpty())
-			{
-				gameOver();
-				return;
-			}
-			//Player One lays down rest of hand and turns last card up
-			if (playerOneHand.size() < 5)
-			{
-				playerOneWarCard = playerOneHand.remove(playerOneHand.size() - 1);
-				playerOneFacedownCards.addAll(playerOneHand);
-				playerOneHand.clear();
-			}
-
-			//Player Two lays down rest of hand and turns last card up
-			if (playerTwoHand.size() < 5)
-			{
-				playerTwoWarCard = playerTwoHand.remove(playerTwoHand.size() - 1);
-				playerTwoFacedownCards.addAll(playerTwoHand);
-				playerTwoHand.clear();
-			}
-
-			if(playerOneWarCard == null)
-			{
-				playerOneFacedownCards.add(playerOneHand.remove(0));
-				playerOneFacedownCards.add(playerOneHand.remove(0));
-				playerOneFacedownCards.add(playerOneHand.remove(0));
-				playerOneWarCard = playerOneHand.remove(0);
-			}
-
-			if(playerTwoWarCard == null)
-			{
-				playerTwoFacedownCards.add(playerTwoHand.remove(0));
-				playerTwoFacedownCards.add(playerTwoHand.remove(0));
-				playerTwoFacedownCards.add(playerTwoHand.remove(0));
-				playerTwoWarCard = playerTwoHand.remove(0);
-			}
-			//If the 2 compared cards are equal, call warHandler(cardone, cardtwo);
-			//RECURSIVE CASE
-			if(playerTwoWarCard.getValue() == playerOneWarCard.getValue())
-			{
-				warHandler(playerOneWarCard, playerTwoWarCard);
-			}
-			//else we now compare whose card is bigger
-			else if(playerTwoWarCard.getValue() > playerOneWarCard.getValue())
-			{
-				//add all the cards to player two
-				playerTwoHand.addAll(playerOneFacedownCards);
-				playerTwoHand.addAll(playerTwoFacedownCards);
-			}
-			else
-			{
-				//playerOne should get all the cards
-				playerOneHand.addAll(playerOneFacedownCards);
-				playerOneHand.addAll(playerTwoFacedownCards);
-			}
-			//clear the war cards for next war
-			playerOneFacedownCards.clear();
-			playerTwoFacedownCards.clear();
-
-
+			gameOver();
+			return;
 		}
+		//Player One lays down rest of hand and turns last card up
+		if (playerOneHand.size() < 5)
+		{
+			playerOneWarCard = playerOneHand.remove(playerOneHand.size() - 1);
+			playerOneFacedownCards.addAll(playerOneHand);
+			playerOneHand.clear();
+		}
+
+		//Player Two lays down rest of hand and turns last card up
+		if (playerTwoHand.size() < 5)
+		{
+			playerTwoWarCard = playerTwoHand.remove(playerTwoHand.size() - 1);
+			playerTwoFacedownCards.addAll(playerTwoHand);
+			playerTwoHand.clear();
+		}
+
+		if(playerOneWarCard == null)
+		{
+			playerOneFacedownCards.add(playerOneHand.remove(0));
+			playerOneFacedownCards.add(playerOneHand.remove(0));
+			playerOneFacedownCards.add(playerOneHand.remove(0));
+			playerOneWarCard = playerOneHand.remove(0);
+		}
+
+		if(playerTwoWarCard == null)
+		{
+			playerTwoFacedownCards.add(playerTwoHand.remove(0));
+			playerTwoFacedownCards.add(playerTwoHand.remove(0));
+			playerTwoFacedownCards.add(playerTwoHand.remove(0));
+			playerTwoWarCard = playerTwoHand.remove(0);
+		}
+		//If the 2 compared cards are equal, call warHandler(cardone, cardtwo);
+		//RECURSIVE CASE
+		if(playerTwoWarCard.getValue() == playerOneWarCard.getValue())
+		{
+			warHandler(playerOneWarCard, playerTwoWarCard);
+		}
+		//else we now compare whose card is bigger
+		else if(playerTwoWarCard.getValue() > playerOneWarCard.getValue())
+		{
+			//add all the cards to player two
+			playerTwoHand.addAll(playerOneFacedownCards);
+			playerTwoHand.addAll(playerTwoFacedownCards);
+		}
+		else
+		{
+			//playerOne should get all the cards
+			playerOneHand.addAll(playerOneFacedownCards);
+			playerOneHand.addAll(playerTwoFacedownCards);
+		}
+		//clear the war cards for next war
+		playerOneFacedownCards.clear();
+		playerTwoFacedownCards.clear();
+
+
+	}
 
 }
